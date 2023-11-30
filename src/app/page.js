a09e3f946f53649db9aa65b2007e64b89f03e885
DON <user@Don.local>
2023-11-30 15:54:42 +0100

    third

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Nav from "@/components/Nav";
 export default function Home() {
   return (
     <main className="snap-y snap-mandatory h-screen w-screen overflow-scroll scroll-smooth ">
-      <div className="sticky top-8 z-10">
+      <div className="sticky top-12 z-10">
         <Nav />
       </div>
 
@@ -14,7 +14,7 @@ export default function Home() {
         id="bg1"
         class="snap-start min-h-screen flex justify-center relative items-center  bg-fixed bg-no-repeat bg-cover bg-center "
       >
-        <h1 className="text-white z-10">Hi 👋🏽 , I’m Tega</h1>
+        <h1 className="text-white z-10 text-xl">Hi 👋🏽 , I’m Tega</h1>
         <div className="absolute inset-0 bg-black opacity-80"></div>
       </section>
 
@@ -22,7 +22,7 @@ export default function Home() {
         id="bg2"
         className="snap-start min-h-screen flex justify-center relative items-center  bg-fixed bg-no-repeat bg-cover bg-center"
       >
-        <h1 className="text-white z-10 w-[80%] md:w-[40%] text-center">
+        <h1 className="text-white z-10 w-[80%] md:w-[40%] text-center text-xl">
           I’m a designer from Lagos, Nigeria. When I’m not designing I’m either
           sleeping, reading or trying out a meal. I enjoy exploring, could be
           with colors, places, fonts, skincare or restaurants. Welcome to my
@@ -33,7 +33,7 @@ export default function Home() {
       <section
         id="bg3"
         class="snap-start min-h-screen flex justify-center relative items-center  bg-fixed bg-no-repeat bg-cover bg-center"
-      ><h1 className="text-white z-10 w-[80%] md:w-[40%] text-center">You get to see a few of my favourite projects, some from a few years back 
+      ><h1 className="text-white z-10 w-[80%] md:w-[40%] text-xl text-center">You get to see a few of my favourite projects, some from a few years back 
         and others more recently designed.  Once again welcome to my portfolio, happy touring! ✌🏽</h1>
         <div className="absolute inset-0 bg-black opacity-60"></div>
       </section>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,11 +5,11 @@ const Nav = () => {
   return (
 
     <section >
-    <div className=' container my-10 flex justify-between'>
+    <div className=' container my-14 flex justify-between'>
 <div>
 <Image src={to} alt='to'/>
 </div>
-<div className='space-x-10 text-white flex '>
+<div className='space-x-16 text-white flex '>
     <p>RESUME</p>
     <p>PROJECTS</p>
     <p>CONTACT ME</p>
