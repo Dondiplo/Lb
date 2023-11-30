39e8fd1688b92df3b58ba9ea3eb5a70d7c8bcd57
DON <user@Don.local>
2023-11-30 16:14:31 +0100

    4

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,24 @@
 import Image from "next/image";
 import "./Home.css";
 import Nav from "@/components/Nav";
+import { GoArrowUp } from "react-icons/go";
+import { GoArrowDown } from "react-icons/go";
+import arrow from "public/arrow.svg"
 
 export default function Home() {
   return (
-    <main className="snap-y snap-mandatory h-screen w-screen overflow-scroll scroll-smooth ">
+    <main className="snap-y snap-mandatory h-screen w-screen overflow-scroll scroll-smooth  ">
       <div className="sticky top-12 z-10">
         <Nav />
       </div>
-
+<div className="z-10  opacity-[60%] fixed top-[50%] right-10 ">
+  <Image src={arrow} alt="arrow"/>
+</div>
       <section
         id="bg1"
         class="snap-start min-h-screen flex justify-center relative items-center  bg-fixed bg-no-repeat bg-cover bg-center "
       >
-        <h1 className="text-white z-10 text-xl">Hi 👋🏽 , I’m Tega</h1>
+        <h1 className="text-white z-10 text-xl font-light">Hi 👋🏽 , I’m Tega</h1>
         <div className="absolute inset-0 bg-black opacity-80"></div>
       </section>
 
@@ -22,20 +27,20 @@ export default function Home() {
         id="bg2"
         className="snap-start min-h-screen flex justify-center relative items-center  bg-fixed bg-no-repeat bg-cover bg-center"
       >
-        <h1 className="text-white z-10 w-[80%] md:w-[40%] text-center text-xl">
+        <h1 className="text-white z-10 w-[80%] md:w-[40%] text-center font-light text-xl">
           I’m a designer from Lagos, Nigeria. When I’m not designing I’m either
           sleeping, reading or trying out a meal. I enjoy exploring, could be
           with colors, places, fonts, skincare or restaurants. Welcome to my
-          portfolio 🤩.{" "}
+          portfolio 🤩.
         </h1>
-        <div className="absolute inset-0 bg-black opacity-60"></div>
+        <div className="absolute inset-0 bg-black opacity-90"></div>
       </section>
       <section
         id="bg3"
         class="snap-start min-h-screen flex justify-center relative items-center  bg-fixed bg-no-repeat bg-cover bg-center"
-      ><h1 className="text-white z-10 w-[80%] md:w-[40%] text-xl text-center">You get to see a few of my favourite projects, some from a few years back 
+      ><h1 className="text-white z-10 w-[80%] md:w-[40%] font-light text-xl text-center">You get to see a few of my favourite projects, some from a few years back 
         and others more recently designed.  Once again welcome to my portfolio, happy touring! ✌🏽</h1>
-        <div className="absolute inset-0 bg-black opacity-60"></div>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
       </section>
     </main>
   );
