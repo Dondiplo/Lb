c4077d764c79cfb6575d052a5aae2cd4d0a156ef
DON <user@Don.local>
2023-12-02 22:13:20 +0100

    hm

--- src/app/page.js
+++ src/app/page.js
@@ -14,56 +14,79 @@ import pexels2m from "public/pexels2m.jpg";
 
 export default function Home() {
   return (
-      <main className="snap-y snap-mandatory h-screen w-screen overflow-scroll scroll-smooth">
-  <div>
-    <Nav />
-  </div>
-  <div className="z-10 opacity-[60%] fixed top-[50%] right-5 md:right-10">
-    <Image src={arrow} alt="arrow" />
-  </div>
-  <section
-    id="bg"
-    className="snap-start min-h-screen flex justify-center relative items-center bg-fixed bg-no-repeat bg-cover bg-center"
-    // style={{ backgroundImage: "url(/pexel1.png)" }}
-  >
-    <Image src={pexel} alt="pexel" className="hidden md:block md:h-screen" />
-    <Image src={pexelsm} alt="pexel" className="h-screen md:hidden md:h-screen" />
-    <h1 className="absolute text-white z-10 text-xl font-light">
-      Hi 👋🏽, I’m Tega
-    </h1>
-    <div className="absolute inset-0 bg-black opacity-80"></div>
-  </section>
+    <main className="snap-y snap-mandatory h-screen w-screen overflow-scroll scroll-smooth">
+      <div>
+        <Nav />
+      </div>
+      <div className="z-10 opacity-[60%] fixed top-[50%] right-5 md:right-10">
+        <Image src={arrow} alt="arrow" />
+      </div>
+      <section
+        id="bg"
+        className="snap-start min-h-screen flex justify-center relative items-center bg-fixed bg-no-repeat bg-cover bg-center"
+        // style={{ backgroundImage: "url(/pexel1.png)" }}
+      >
+        <Image
+          src={pexel}
+          alt="pexel"
+          className="hidden md:block md:h-screen"
+        />
+        <Image
+          src={pexelsm}
+          alt="pexel"
+          className="h-screen md:hidden md:h-screen"
+        />
+        <h1 className="absolute text-white z-10 text-xl font-light">
+          Hi 👋🏽, I’m Tega
+        </h1>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+      </section>
 
-  <section
-    id="bg"
-    className="snap-start min-h-screen flex justify-center relative items-center bg-fixed bg-no-repeat bg-cover bg-center"
-    // style={{ backgroundImage: "url(/pexels.jpg)" }}
-  >
-    <Image src={pexels} alt="pexels" className="hidden md:block md:h-screen" />
-    <Image src={pexels1m} alt="pexels1m" className="h-screen md:hidden md:h-screen" />
-    <h1 className="text-white absolute z-10 w-[80%] md:w-[40%] text-center font-light text-xl">
-      I’m a designer from Lagos, Nigeria. When I’m not designing I’m either
-      sleeping, reading or trying out a meal. I enjoy exploring, could be
-      with colors, places, fonts, skincare or restaurants. Welcome to my
-      portfolio 🤩.
-    </h1>
-    <div className="absolute inset-0 bg-black opacity-90"></div>
-  </section>
+      <section
+        id="bg"
+        className="snap-start min-h-screen flex justify-center relative items-center bg-fixed bg-no-repeat bg-cover bg-center"
+        // style={{ backgroundImage: "url(/pexels.jpg)" }}
+      >
+        <Image
+          src={pexels}
+          alt="pexels"
+          className="hidden md:block md:h-screen"
+        />
+        <Image
+          src={pexels1m}
+          alt="pexels1m"
+          className="h-screen md:hidden md:h-screen"
+        />
+        <h1 className="text-white absolute z-10 w-[80%] md:w-[40%] text-center font-light text-xl">
+          I’m a designer from Lagos, Nigeria. When I’m not designing I’m either
+          sleeping, reading or trying out a meal. I enjoy exploring, could be
+          with colors, places, fonts, skincare or restaurants. Welcome to my
+          portfolio 🤩.
+        </h1>
+        <div className="absolute inset-0 bg-black opacity-90"></div>
+      </section>
 
-  <section
-    id="bg3"
-    className="snap-start min-h-screen flex justify-center relative items-center bg-fixed bg-no-repeat bg-cover bg-center"
-  >
-    <Image src={pexels2} alt="pexels2" className="hidden md:block md:h-screen" />
-    <Image src={pexels2m} alt="pexels2m" className="h-screen md:hidden md:h-screen" />
-    <h1 className="text-white absolute z-10 w-[80%] md:w-[40%] font-light text-xl text-center">
-      You get to see a few of my favourite projects, some from a few years
-      back and others more recently designed. Once again welcome to my
-      portfolio, happy touring! ✌🏽
-    </h1>
-    <div className="absolute inset-0 bg-black opacity-80"></div>
-  </section>
-
+      <section
+        id="bg3"
+        className="snap-start min-h-screen flex justify-center relative items-center bg-fixed bg-no-repeat bg-cover bg-center"
+      >
+        <Image
+          src={pexels2}
+          alt="pexels2"
+          className="hidden md:block md:h-screen"
+        />
+        <Image
+          src={pexels2m}
+          alt="pexels2m"
+          className="h-screen md:hidden md:h-screen"
+        />
+        <h1 className="text-white absolute z-10 w-[80%] md:w-[40%] font-light text-xl text-center">
+          You get to see a few of my favourite projects, some from a few years
+          back and others more recently designed. Once again welcome to my
+          portfolio, happy touring! ✌🏽
+        </h1>
+        <div className="absolute inset-0 bg-black opacity-80"></div>
+      </section>
 
       {/* <span
       style={{transform:`translateX(${50-100}%)`}}
