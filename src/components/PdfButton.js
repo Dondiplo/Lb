d906690ede03237fd15258c245de9fd8f8c8c594
DON <user@Don.local>
2023-12-11 21:51:29 +0100

    hm,

--- src/components/PdfButton.js
+++ src/components/PdfButton.js
@@ -3,7 +3,7 @@ import React from "react";
 import Image from "next/image";
 
 const Pdf = () => {
-  return <div className="h-[100dvh]">
+  return <div className="h-[100vh]">
   
   <iframe src="/Tegaresume.pdf" width="100%" height="100%" />
 </div>
