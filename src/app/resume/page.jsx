0db9a2363ae0e79df0b0748678f562d48d926e7d
DON <user@Don.local>
2023-12-12 09:49:08 +0100

    f

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div className="h-[100%] md:h-[100vh]">
+    <div className="h-[100dvh] md:h-[100vh] w-full">
       <iframe src="/Tegaresume.pdf" width="100%" height="100%" className="" />
     </div>
   );
